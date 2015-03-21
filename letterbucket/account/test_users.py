7ef895e5899deccc73b4d033325322b66033b2e9
xorinox <xorinox@gmx.ch>
2015-03-21 14:14:03 -0400

    testing get user by id and email

--- letterbucket/account/test_users.py
+++ letterbucket/account/test_users.py
@@ -1,10 +1,10 @@
-import faker
-import unittest
-
-import users
+from .. import create_application
+from .. import db
 from ..config import test
 from ..utilities import testing
-from .. import create_application, db
+import faker
+import unittest
+import users
 
 
 class UserModelTest(testing.DbModelTestCase):
@@ -72,6 +72,23 @@ class UserModelTest(testing.DbModelTestCase):
             second_user.Persist()
             self.assertEqual([new_user, second_user], users.User.query.all(), 'Both users should be in the db.')
 
+    def testGetUserById(self):
+        test_user_username = self.fake_data.user_name()
+        test_user_name = self.fake_data.name()
+        test_user_email = self.fake_data.email()
+        test_user_password = self.fake_data.word()
+        with self.app.test_request_context():
+            self.assertEqual([], users.User.query.all(), 'Should be no users in the db.')
+            new_user = users.User(test_user_username,
+                                  test_user_email,
+                                  test_user_name,
+                                  test_user_password)
+            new_user.Persist()            
+            good_user = users.User.GetById(new_user.id)
+            bad_user = users.User.GetById(new_user.id + 1)
+            self.assertEqual([new_user], [good_user], 'User should be the same as in the db.')
+            self.assertNotEqual([new_user], [bad_user], 'User should not be the same as in the db.')
+
     def testGetUserByUsername(self):
         test_user_username = self.fake_data.user_name()
         test_user_name = self.fake_data.name()
@@ -89,6 +106,23 @@ class UserModelTest(testing.DbModelTestCase):
             self.assertEqual([new_user], [good_user], 'User should be the same as in the db.')
             self.assertNotEqual([new_user], [bad_user], 'User should not be the same as in the db.')
 
+    def testGetUserByEmail(self):
+        test_user_username = self.fake_data.user_name()
+        test_user_name = self.fake_data.name()
+        test_user_email = self.fake_data.email()
+        test_user_password = self.fake_data.word()
+        with self.app.test_request_context():
+            self.assertEqual([], users.User.query.all(), 'Should be no users in the db.')
+            new_user = users.User(test_user_username,
+                                  test_user_email,
+                                  test_user_name,
+                                  test_user_password)
+            new_user.Persist()            
+            good_user = users.User.GetByEmail(test_user_email)
+            bad_user = users.User.GetByEmail(self.fake_data.email())
+            self.assertEqual([new_user], [good_user], 'User should be the same as in the db.')
+            self.assertNotEqual([new_user], [bad_user], 'User should not be the same as in the db.')
+    
     def testUserPasswords(self):
         with self.app.test_request_context():
             test_password = self.fake_data.word()
