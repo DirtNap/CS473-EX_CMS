5da34a534fd164e2c55f256f4e6917b698204d78
ryangold@bu.edu <ryangold@bu.edu>
2015-03-26 20:37:44 -0400

    Test method changes

--- letterbucket/content/test_blogs.py
+++ letterbucket/content/test_blogs.py
@@ -3,7 +3,10 @@ import unittest
 
 import blogs
 
-from account import users
+from .. import db, create_application
+from ..account import users
+from ..config import test
+from ..utilities import testing
 
 class BlogModelTest(testing.DbModelTestCase):
 
@@ -16,17 +19,22 @@ class BlogModelTest(testing.DbModelTestCase):
         
     
     def testCreateBlog(self):
-        test_blog_path = self.fake_data.path()
+        test_blog_path = self.fake_data.uri_path()
         test_blog_name = self.fake_data.name()
-        test_user_owner_id = self.fake_owner_id()
+
         with self.app.test_request_context():
-            self.assertEqual([], blogs.Blogs.query.all(), 'Should be no blogs in the db.')
-            new_blog = blogs.Blogs(test_blog_path,
+            self.assertEqual([], blogs.Blog.query.all(), 'Should be no blogs in the db.')
+            new_user = users.User(self.fake_data.user_name(),
+                                  self.fake_data.email(),
+                                  self.fake_data.name(),
+                                  self.fake_data.word())
+            new_user.Persist()
+            new_blog = blogs.Blog(test_blog_path,
                                   test_blog_name,
-                                  test_blog_owner_id)
+                                  new_user)
             self.assertEqual(test_blog_path, new_blog.path, 'Paths should match')
-            self.assertEqual(test_blog_name, new_blog.path, 'Names should match')
-            self.assertEqual(test_blog_user_id, new_blog.user_id, 'user_id addresses should match')
+            self.assertEqual(test_blog_name, new_blog.name, 'Names should match')
+            self.assertEqual(new_user.id, new_blog.owner_id, 'user_id addresses should match')
             self.assertEqual([], blogs.Blog.query.all(), 'Should be no blogs in the db.')
             self.assertFalse(new_blog.IsPersisted(), 'Blog has not yet been committed.')
             new_blog = blogs.Blog(self.fake_data.uri_page(),
@@ -46,36 +54,40 @@ class BlogModelTest(testing.DbModelTestCase):
                                   self.fake_data.word())
             new_user.Persist()            
             
-            new_blog = blogs.Blog(self.fake_data.path(),
+            no2_user = users.User(self.fake_data.user_name(),
+                                  self.fake_data.email(),
                                   self.fake_data.name(),
-                                  self.fake_data.user_id())
+                                  self.fake_data.word())
+            no2_user.Persist()            
+            
+            new_blog = blogs.Blog(self.fake_data.uri_path(),
+                                  self.fake_data.name(),
+                                  new_user)
+            new_blog.Persist()                      
             self.assertEqual([new_blog], blogs.Blog.query.all(), 'Blog should be in the db.')
-          
-            second_blog = blogs.Blog(self.fake_data.path(),
+                                  
+            second_blog = blogs.Blog(self.fake_data.uri_path(),
                                      self.fake_data.name(),
-                                     self.fake_data.user_id())
+                                     no2_user)
             second_blog.path = new_blog.path
-            
-            
-            self._AssertConstraintError(second_blog.Persist, 'UNIQUE', 'path',
+            self._AssertConstraintError(db, self.UNIQUE_ASSERTION_RE, 'path',
+                                        second_blog.Persist,
                                         msg='Path must be unique.')
             second_blog.path = self.fake_data.uri_path()
 
-            second_blog.owner_id = new_user.id            
-            self._AssertConstraintError(second_blog.Persist, 'UNIQUE', 'owner_id',
-                                        msg='User_ID must be unique.')
-            second_blog.owner_id = second_user.id()            
+            second_blog.owner_id = new_blog.owner_id            
+            self._AssertConstraintError(db, self.UNIQUE_ASSERTION_RE, 'owner_id',
+                                        second_blog.Persist,
+                                        msg='Owner_ID must be unique.')
+            second_blog.owner_id = no2_user.id
 
-            second_user.name = None
-            self._AssertConstraintError(second_user.Persist, 'NOT NULL', 'name',
+            second_blog.name = None
+            self._AssertConstraintError(db, self.NOT_NULL_ASSERTION_RE, 'name',
+                                        second_blog.Persist,
                                         msg='Name must not be null.')
-            second_user.name = self.fake_data.name()
-            
-            second_blog.owner_id = second_user.id + 10 # should not exist
+            second_blog.name = self.fake_data.name()
             second_blog.Persist()
-            
-            second_user.Persist()
-            self.assertEquals([new_user, second_user], users.User.query.all(), 'Both users should be in the db.')
+            self.assertEquals([new_blog, second_blog], blogs.Blog.query.all(), 'Both users should be in the db.')
 
         
 if __name__ == '__main__':
